ea83b3f97235ac839972695063b951690c7de9c3
Michał Januszko <michal@januszko.net>
2024-03-03 19:22:23 +0100

    Add Serilog

--- src/DocDocs.WebApi/Program.cs
+++ src/DocDocs.WebApi/Program.cs
@@ -1,6 +1,45 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using Serilog;
 
-app.MapGet("/", () => "DocDocs API");
+namespace DocDocs.WebApi
+{
+    public class Program
+    {
+        private const string AppName = "DocDocs.WebApi";
 
-app.Run();
+        public static void Main(string[] args)
+        {
+            Log.Logger = new LoggerConfiguration()
+                .Enrich.WithProperty("Application", AppName)
+                .Enrich.WithProperty("MachineName", Environment.MachineName)
+                .Enrich.FromLogContext()
+                .WriteTo.Console()
+                .CreateBootstrapLogger();
+
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Host.UseSerilog((context, services, configuration) => configuration
+                .Enrich.WithProperty("Application", AppName)
+                .Enrich.WithProperty("MachineName", Environment.MachineName)
+                .ReadFrom.Configuration(context.Configuration)
+                .ReadFrom.Services(services)
+                .Enrich.FromLogContext());
+
+            builder.Services.AddControllers();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
